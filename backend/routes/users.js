d37dd4d1bc402f4a809100cd8a73c008c47f55e5
Kat Mota <katmmota@gmail.com>
2025-03-15 17:01:59 -0500

    assign admin role, add a review route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -36,3 +36,62 @@ router.post("/register", async (req, res) => {
 });
 
 //user login and token generation
+router.post("login", async (req,res) => {
+    const {email, password} = req.body;
+
+    try {
+        const userRes = await pool.query(`SELECT * FROM users WHERE email = $1`, [email]);
+        if (userRes.rows.length === 0 ) return res.status(401).json({error: "Invalid credentials"});
+
+        const user = userRes.rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(401).json({ error: "Invalid credentials"});
+
+        const token = jwt.sign({ id: user.id, is_admin: user.is_admin}, process.env.JST_SECRET, { expiresIn: "2d"});
+
+        res.json({message: "Login successful", token});
+    }catch(error) {
+        console.error("Error logging in:", error);
+        res.status(500).json({error: "Internal Server Error:"});
+    }
+});
+
+//get current user profile
+router.get("/me", authenticateUser, async (req,res) => {
+    try {
+        const result = await pool.query(`SELECT id, name, email, is_admin FROM users WHERE is = $1`, [req.user.id]); 
+        if (result.rows.length === 0) return res.status(404).json({error: "User not found"});
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+});
+
+//ADMIN ONLY: get all users
+router.get("/", authenticateUser, requireAdmin, async (req,res) => {
+    try {
+        const result = await pool.query(`SELECT id, name, email, is_admin FROM users`);
+        res.json(result.rows);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+});
+
+//ADMIN ONLY: assign admin role
+router.patch("/:id/admin", authenticateUser, requireAdmin, async (req,res) => {
+    const {id} = req.params;
+
+    try {
+        await pool.query(`UPDATE users SET is_admin = TRUE WHERE id = $1`, [id]);
+        res.json({message: "User promoted to admin success"});
+    } catch (error) {
+        console.error("Error updating user:", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+});
+
+module.exports = router;
+
